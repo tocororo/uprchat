3b6d62374ebe4774685bfc3db3600da7ac3def5b
Jorge Luis Arencibia <jorge9815rdz@gmail.com>
2024-11-15 15:24:22 -0500

    refactor: moved all data to uprchat module

--- uprchat/app/main.py
+++ uprchat/app/main.py
@@ -39,4 +39,4 @@ app.include_router(mapper_router.router)
 
 def start():
     """Launched with `poetry run start` at root level"""
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("uprchat.app.main:app", host="0.0.0.0", port=8000, reload=True)
