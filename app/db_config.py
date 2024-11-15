5d38ee140b2c99f239ea46931c06fca42a768f14
Jorge Luis Arencibia <jorge9815rdz@gmail.com>
2024-11-15 13:53:28 -0500

    fix: added missing dependencies

--- app/db_config.py
+++ app/db_config.py
@@ -1,17 +1,14 @@
 from sqlmodel import create_engine,Session
 from .models import SQLModel
-import os
 
-DB_URL = os.getenv("POSTGRES_DATABASE_URL")
+from .config import get_settings
 
-print(DB_URL)
-
-# engine = create_engine(DB_URL)
+engine = create_engine(get_settings().postgres_database_url)
 
 
-# def create_db_and_tables():
-#     SQLModel.metadata.create_all(engine)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
-# def get_session():
-#     with Session(engine) as session:
-#         yield session
+def get_session():
+    with Session(engine) as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 from fastapi import FastAPI
-# from .db_config import create_db_and_tables
+from .db_config import create_db_and_tables
 from .routes import urls, domains, sources, models, collectors, jobs, users, chats,llmqueries
 from .routes.mapper import mapper_router
 
@@ -10,20 +10,20 @@ app = FastAPI(title="UPR-K API", version="1.0.0")
 
 @app.get("/")
 def root():
-    # create_db_and_tables()
+    create_db_and_tables()
     return "Tables created"
 
 
 
-# app.include_router(urls.rt)
-# app.include_router(domains.rt)
-# app.include_router(sources.rt)
-# app.include_router(models.rt)
-# app.include_router(collectors.rt)
-# app.include_router(jobs.rt)
-# app.include_router(users.rt)
-# app.include_router(chats.rt)
-# app.include_router(llmqueries.rt)
+app.include_router(urls.rt)
+app.include_router(domains.rt)
+app.include_router(sources.rt)
+app.include_router(models.rt)
+app.include_router(collectors.rt)
+app.include_router(jobs.rt)
+app.include_router(users.rt)
+app.include_router(chats.rt)
+app.include_router(llmqueries.rt)
 
 app.include_router(mapper_router.router)
 
